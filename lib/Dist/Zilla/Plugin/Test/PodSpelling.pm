f65b3c98a65e74d6c6f504b09c13b3ef57212bda
Caleb Cushing <xenoterracide@gmail.com>
2011-10-08 22:49:24 -0500

    add an acknowledgment
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Dist/Zilla/Plugin/Test/PodSpelling.pm
+++ lib/Dist/Zilla/Plugin/Test/PodSpelling.pm
@@ -136,6 +136,16 @@ C<dist.ini> are automatically added as stopwords so you don't have to add them
 manually just because they might appear in the C<AUTHORS> section of the
 generated POD document.
 
+=head1 ACKNOWLEDGEMENTS
+
+=over
+
+=item * Randy Stauner
+
+providing initial tests
+
+=back
+
 =begin Pod::Coverage
 
 mvp_multivalue_args
