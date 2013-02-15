2cca69c73fb1fff1dd883d678b4864936e0be265
Caleb Cushing <xenoterracide@gmail.com>
2013-02-14 21:50:59 -0600

    readd bin to default directories
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Dist/Zilla/Plugin/Test/PodSpelling.pm
+++ lib/Dist/Zilla/Plugin/Test/PodSpelling.pm
@@ -191,5 +191,5 @@ eval "use Test::Spelling 0.12; use {{ $wordlist }}; 1" or die $@;
 
 {{ $set_spell_cmd }}
 {{ $add_stopwords }}
-all_pod_files_spelling_ok( qw( lib {{ $directories }} ) );
+all_pod_files_spelling_ok( qw( bin lib {{ $directories }} ) );
 {{ $stopwords }}
